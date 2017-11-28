a3d8c982e29049d6a8de756c17a411d148c17e0e
samirsahu35 <34069070+samirsahu35@users.noreply.github.com>
2017-11-28 22:21:32 +0530

    Removed unused class/package imports and lines

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.List;
 
 public class FlightBookingTest {
